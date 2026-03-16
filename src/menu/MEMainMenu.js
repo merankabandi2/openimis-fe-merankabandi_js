93dfcbf47f9efb112be038ab0f0226c3eb0aa5af
Anthony BELEM <anthbel@gmail.com>
2026-03-16 23:48:30 +0200

    Fix S&E menu: add missing translation, remove duplicate entries
    - Add missing "mainMenu.me" → "Suivi-Evaluation" French translation
    - Remove hardcoded menu entries from MEMainMenu (duplicated index.js contributions with wrong translation keys)
    - Fix "Tableau de Bord des Activités" → "Tableau de Bord - Activités"

--- src/menu/MEMainMenu.js
+++ src/menu/MEMainMenu.js
@@ -1,55 +1,13 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/jsx-props-no-spreading */
-
 import React from 'react';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
-import { Assessment, BarChart, AttachMoney, Dashboard } from '@material-ui/icons';
 import { formatMessage, MainMenuContribution, withModulesManager } from '@openimis/fe-core';
-import {
-  RIGHT_BENEFIT_PLAN_SEARCH,
-  ME_MAIN_MENU_CONTRIBUTION_KEY,
-  ROUTE_ME_MONETARY_TRANSFERS,
-  ROUTE_ME_RESULT_FRAMEWORK,
-  RIGHT_MONETARY_TRANSFER_SEARCH,
-} from '../constants';
+import { ME_MAIN_MENU_CONTRIBUTION_KEY } from '../constants';
 
 function MEMainMenu(props) {
-  const entries = [
-    {
-      text: formatMessage(props.intl, 'merankabandi', 'menu.merankabandi.resultFrameWork'),
-      icon: <Assessment />,
-      route: `/${ROUTE_ME_RESULT_FRAMEWORK}`,
-      filter: (rights) => rights.includes(RIGHT_MONETARY_TRANSFER_SEARCH),
-      id: 'merankabandi.me.resultFrameWork',
-    },
-    {
-      text: formatMessage(props.intl, 'merankabandi', 'menu.merankabandi.indicators'),
-      icon: <BarChart />,
-      route: '/me/indicators',
-      filter: (rights) => rights.includes(RIGHT_BENEFIT_PLAN_SEARCH),
-      id: 'merankabandi.me.indicators',
-    },
-    {
-      text: formatMessage(props.intl, 'merankabandi', 'menu.merankabandi.monetaryTransfer'),
-      icon: <AttachMoney />,
-      route: `/${ROUTE_ME_MONETARY_TRANSFERS}`,
-      filter: (rights) => rights.includes(RIGHT_MONETARY_TRANSFER_SEARCH),
-      id: 'merankabandi.me.monetaryTransfers',
-    },
-    {
-      text: formatMessage(props.intl, 'merankabandi', 'menu.merankabandi.enhancedResultFramework'),
-      icon: <Dashboard />,
-      route: '/me/enhanced-results-framework',
-      filter: (rights) => rights.includes(RIGHT_MONETARY_TRANSFER_SEARCH),
-      id: 'merankabandi.me.enhancedResultFramework',
-    },
-  ];
-  entries.push(
-    ...props.modulesManager
-      .getContribs(ME_MAIN_MENU_CONTRIBUTION_KEY)
-      .filter((c) => !c.filter || c.filter(props.rights)),
-  );
+  const entries = props.modulesManager
+    .getContribs(ME_MAIN_MENU_CONTRIBUTION_KEY)
+    .filter((c) => !c.filter || c.filter(props.rights));
 
   return (
     <MainMenuContribution
