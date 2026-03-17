fe755fcae1ce35e6c976a9687aa8246b19bd1350
Anthony BELEM <anthbel@gmail.com>
2026-03-17 08:25:12 +0200

    Improve CardGenerationButton with Material-UI styling and loading state

--- src/components/social-protection/CardGenerationButton.js
+++ src/components/social-protection/CardGenerationButton.js
@@ -1,36 +1,47 @@
-import React from 'react';
-import { CARD_GENERATION_URL } from '../../constants';
+import React, { useState } from 'react';
+import { Button, CircularProgress } from '@material-ui/core';
+import CreditCardIcon from '@material-ui/icons/CreditCard';
+import { injectIntl } from 'react-intl';
+import { formatMessage } from '@openimis/fe-core';
+import { MODULE_NAME, CARD_GENERATION_URL } from '../../constants';
 
-function CardGenerationButton({ location, formatMessage }) {
+function CardGenerationButton({ intl, location }) {
+  const [loading, setLoading] = useState(false);
+
   if (!location || !location.countActive || location.countActive <= 0) {
     return null;
   }
 
-  const handleGenerateCards = (e) => {
-    e.preventDefault();
+  const handleGenerateCards = () => {
+    setLoading(true);
     const url = `${CARD_GENERATION_URL}/${location.id}/generate-cards-background/`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
-          alert(formatMessage('cards.generationStarted'));
+          alert(formatMessage(intl, MODULE_NAME, 'cards.generationStarted'));
         } else {
-          alert(`${formatMessage('cards.generationError')}: ${data.message}`);
+          alert(`${formatMessage(intl, MODULE_NAME, 'cards.generationError')}: ${data.message}`);
         }
       })
       .catch((error) => {
-        alert(`${formatMessage('cards.generationError')}: ${error.message}`);
-      });
+        alert(`${formatMessage(intl, MODULE_NAME, 'cards.generationError')}: ${error.message}`);
+      })
+      .finally(() => setLoading(false));
   };
 
   return (
-    <a
-      href="#"
+    <Button
+      variant="outlined"
+      size="small"
+      startIcon={loading ? <CircularProgress size={16} /> : <CreditCardIcon />}
       onClick={handleGenerateCards}
+      disabled={loading}
     >
-      {formatMessage('cards.generate')}
-    </a>
+      {formatMessage(intl, MODULE_NAME, 'cards.generate')}
+      {` (${location.countActive})`}
+    </Button>
   );
 }
 
-export default CardGenerationButton;
+export default injectIntl(CardGenerationButton);
